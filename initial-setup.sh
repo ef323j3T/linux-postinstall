ae4190a898dffcba9755e252c2a24da57a17ae31
em <53843689+ef323j3T@users.noreply.github.com>
2019-12-01 19:15:31 +1100

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -21,3 +21,133 @@ install_tools(){
         clear
         print_status "Install Tools"
         apt-get -y  install git vim nano make gcc curl wget net-tools zsh ; }
+
+
+function addUserAccount() {
+# Add the new user account. Args: 'Username', 'password'. Flag to determine if user account is added silently. (With / Without GECOS prompt)
+    	local username=${1}
+    	local password=${2}
+    	local silent_mode=${3}
+    	if [[ ${silent_mode} == "true" ]]; then
+        	sudo adduser  --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password --gecos '' "${username}"
+    	else
+        	sudo adduser  --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password "${username}"
+    	fi
+    	echo "${username}:${password}" | sudo chpasswd
+    	sudo usermod -aG sudo "${username}" ; }
+
+
+function addSSHKey() {
+# Add the local machine public SSH Key for the new user account. Args: 'username', 'public ssh key'
+	local username=${1}
+    	local sshKey=${2}
+    	local GITMAIL=${3}
+	local GITUSER=${4}
+	local GITPASS=${5}
+
+    	execAsUser "${username}" "mkdir -p ~/.ssh; chmod 700 ~/.ssh; touch ~/.ssh/authorized_keys"
+    	execAsUser "${username}" "echo \"${sshKey}\" | sudo tee -a ~/.ssh/authorized_keys"
+    	execAsUser "${username}" "chmod 600 ~/.ssh/authorized_keys"
+    	execAsUser "${username}" "ssh-keygen -t rsa -b 4096 -C '${GITMAIL}' -N '' -f ~/.ssh/id_rsa"
+
+function execAsUser() {
+# Execute a command as a certain user. Args: 'username', 'command to be executed'
+    	local username=${1}
+    	local exec_command=${2}
+    	sudo -u "${username}" -H bash -c "${exec_command}" ; }
+
+function changeSSHConfig() { 
+# Modify the sshd_config file. shellcheck disable=2116
+    	sudo sed -re 's/^(\#?)(PasswordAuthentication)([[:space:]]+)yes/\2\3no/' -i."$(echo 'old')" /etc/ssh/sshd_config
+    	sudo sed -re 's/^(\#?)(PermitRootLogin)([[:space:]]+)(.*)/PermitRootLogin no/' -i /etc/ssh/sshd_config ; }
+
+
+function setTimezone() {
+# Set the machines timezone. Args: 'tz data timezone'
+    	local timezone=${1}
+    	echo "${1}" | sudo tee /etc/timezone
+    	sudo ln -fs "/usr/share/zoneinfo/${timezone}" /etc/localtime
+    	sudo dpkg-reconfigure -f noninteractive tzdata ; }
+        
+        
+function configureNTP() { 
+# Configure Network Time Protocol
+    	sudo apt-get update
+    	sudo apt-get --assume-yes install ntp ;}
+
+
+function disableSudoPassword() { 
+# Disables the sudo password prompt for a user account by editing /etc/sudoers. Args: 'username'
+    	local username="${1}"
+    	sudo cp /etc/sudoers /etc/sudoers.bak
+    	sudo bash -c "echo '${1} ALL=(ALL) NOPASSWD: ALL' | (EDITOR='tee -a' visudo)" ; }
+
+
+function revertSudoers() {
+# Reverts the original /etc/sudoers file before this script is ran
+    	sudo cp /etc/sudoers.bak /etc/sudoers
+    	sudo rm -rf /etc/sudoers.bak ; }
+
+function cleanup() {
+    if [[ -f "/etc/sudoers.bak" ]]; then
+        revertSudoers
+    fi ; }
+
+function logTimestamp() {
+    local filename=${1}
+    {
+        echo "==================="
+        echo "Log generated on $(date)"
+        echo "==================="
+    } >>"${filename}" 2>&1 ; }
+
+function setupTimezone() {
+    timezone="Australia/Sydney"
+    setTimezone "${timezone}"
+    echo "Timezone is set to $(cat /etc/timezone)" >&3 ; }
+
+function promptForPassword() {
+#Keep prompting for the password and password confirmation
+   PASSWORDS_MATCH=0
+   while [ "${PASSWORDS_MATCH}" -eq "0" ]; do
+       read -s -rp "Enter new UNIX password:" password
+       printf "\n"
+       read -s -rp "Retype new UNIX password:" password_confirmation
+       printf "\n"
+       if [[ "${password}" != "${password_confirmation}" ]]; then
+           echo "Passwords do not match! Please try again."
+       else
+           PASSWORDS_MATCH=1
+       fi
+   done ; }
+
+
+function main() {
+    	read -rp "Enter the username of the new user account:" username
+    	promptForPassword
+    	read -rp "Enter the mail of git account:" gitmail
+
+    	addUserAccount "${username}" "${password}"
+
+   	read -rp $'Paste in the public SSH key for the new user:\n' sshKey
+    	echo 'Running setup script...'
+    	logTimestamp "${output_file}"
+
+    	exec 3>&1 >>"${output_file}" 2>&1
+    	disableSudoPassword "${username}"
+    	addSSHKey "${username}" "${sshKey}"
+    	changeSSHConfig
+
+    	setupTimezone
+ 	echo "Installing Network Time Protocol... " >&3
+    	configureNTP
+
+    	sudo service ssh restart
+   	cleanup
+   	echo "Setup Done! Log file is located at ${output_file}" >&3
+}
+
+update_ubuntu
+install_tools
+main
+
