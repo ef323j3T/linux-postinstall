47d84532269c135219f24f25ec0447b6b85e4ff2
em <53843689+ef323j3T@users.noreply.github.com>
2019-12-03 13:11:22 +1100

    Update git-setup.sh

--- git-setup.sh
+++ git-setup.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 set -e
 
-#http://unix.stackexchange.com/questions/136894/command-line-method-or-programmatically-add-ssh-key-to-github-com-user-account
-#https://github.com/b4b4r07/ssh-keyreg/blob/master/bin/ssh-keyreg
-#https://github.com/ABCanG/add-sshkey-remote
+# http://unix.stackexchange.com/questions/136894/command-line-method-or-programmatically-add-ssh-key-to-github-com-user-account
+# https://github.com/b4b4r07/ssh-keyreg/blob/master/bin/ssh-keyreg
+# https://github.com/ABCanG/add-sshkey-remote
 
 git_key() {
     local title="${USER}@${HOSTNAME}"
@@ -22,27 +22,23 @@ git_key() {
     mkdir -p ${GIT_USER_DIR} && touch ${GIT_USER_FILE}
     echo -e  "[user]\n     email = $gitMail\n     name = $gitUser" >> ${GIT_USER_FILE} ; }
 
-
 check_git() {
     ssh -T git@github.com 2>&1 | grep "success"
     if [[ $? -ne 0 ]] ; then
         echo "error."
     fi ; }
 
-
 deploy_dotfiles() {
     if [ ! -f ${HOME}/.local/dotfiles/deploy.zsh ] ; then
         git clone git@github.com:ef323j3T/dotfiles.git "${HOME}/.local/dotfiles"
     fi ; }
 
-
 clean_dotfiles () {    
     if [ "$OSTYPE" -ne "darwin" ] ; then
-        rm ${HOME}/.local/dotfiles/zsh/zshrc.d/aliases/chrome.zsh
-        rm ${HOME}/.local/dotfiles/zsh/zshrc.d/aliases/j=jump.zsh
-        rm ${HOME}/.local/dotfiles/zsh/zshrc.d/aliases/ff=find-fast.zsh
+        for files in "${MAC_ZSH_ARRAY[@]}" ; do
+            rm -f "$files" 
+        done
     fi  ; }
-
 
 install_packages() {
     sudo apt-get -y install apt-transport-https ca-certificates \
