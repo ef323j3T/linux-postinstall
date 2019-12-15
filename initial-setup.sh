ac3855758414f557beb25a409ce534a8f96b7f22
em <53843689+ef323j3T@users.noreply.github.com>
2019-12-15 15:33:34 +1100

    Add files via upload

--- initial-setup.sh
+++ initial-setup.sh
@@ -14,9 +14,10 @@ fi
 # setup/blob/3e02daa9420f2ef0c24ccc560aa28df32
 # 2e314d0/setupLibrary.sh
 
-print_status() { echo "$1" ; }
+function print_status() { echo "$1"
+}
 
-addUserAccount() {
+function addUserAccount() {
 # Add the new user account. Args: 'Username', 'password'. Flag to determine if user account is added silently. (With / Without GECOS prompt)
     	local username="${1}"
     	local password="${2}"
@@ -27,7 +28,8 @@ addUserAccount() {
         	sudo adduser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password "${username}"
     	fi
     	echo "${username}:${password}" | sudo chpasswd
-    	sudo usermod -aG sudo "${username}" ; }
+    	sudo usermod -aG sudo "${username}"
+}
 
 addSSHKey() {
 # Add the local machine public SSH Key for the new user account. Args: 'username', 'public ssh key'
@@ -39,58 +41,65 @@ addSSHKey() {
     	execAsUser "${username}" "chmod 600 ~/.ssh/authorized_keys"
     	execAsUser "${username}" "ssh-keygen -t rsa -b 4096 -C '${gitMail}' -N '' -f ~/.ssh/id_rsa" ; }
 
-execAsUser() {
+function execAsUser() {
 # Execute a command as a certain user. Args: 'username', 'command to be executed'
     	local username="${1}"
     	local exec_command="${2}"
-    	sudo -u "${username}" -H bash -c "${exec_command}" ; }
+    	sudo -u "${username}" -H bash -c "${exec_command}"
+}
 
-disableSudoPassword() {
+function disableSudoPassword() {
 # Disables the sudo password prompt for a user account by editing /etc/sudoers. Args: 'username'
     	local username="${1}"
     	sudo cp /etc/sudoers /etc/sudoers.bak
-    	sudo bash -c "echo '${1} ALL=(ALL) NOPASSWD: ALL' | (EDITOR='tee -a' visudo)" ; }
+    	sudo bash -c "echo '${1} ALL=(ALL) NOPASSWD: ALL' | (EDITOR='tee -a' visudo)"
+}
 
-revertSudoers() {
+function revertSudoers() {
 # Reverts the original /etc/sudoers file before this script is ran
 	sudo cp /etc/sudoers.bak /etc/sudoers
-    	sudo rm -rf /etc/sudoers.bak ; }
+    	sudo rm -rf /etc/sudoers.bak
+}
 
-logTimestamp() {
-	sed -i 's|'${1}'=.*|'${1}'='${TIMESTAMP}'|g' $LOGFILE ; }
+function logTimestamp() {
+	sed -i 's|'${1}'=.*|'${1}'='${TIMESTAMP}'|g' $LOGFILE
+}
 
-setTimezone() {
+function setTimezone() {
 # Set the machines timezone. Args: 'tz data times'
     local timezone="${1}"
     echo "${1}" | sudo tee /etc/timezone
     sudo ln -fs "/usr/share/zoneinfo/${timezone}" /etc/localtime # https://bugs.launchpad.net/ubuntu/+source/tzdata/+bug/1554806
-    sudo dpkg-reconfigure -f noninteractive tzdata ; }
+    sudo dpkg-reconfigure -f noninteractive tzdata
+}
 
-promptForPassword() {
+function promptForPassword() {
 #Keep prompting for the password and password confirmation
    PASSWORDS_MATCH=0
-   while [ "${PASSWORDS_MATCH}" -eq "0" ] ; do
+   while [ "${PASSWORDS_MATCH}" -eq "0" ]; do
        read -s -rp "Enter new UNIX password:" password
        printf "\n"
        read -s -rp "Retype new UNIX password:" password_confirmation
        printf "\n"
 
-       if [[ "${password}" != "${password_confirmation}" ]] ; then
+       if [[ "${password}" != "${password_confirmation}" ]]; then
            echo "Passwords do not match! Please try again."
        else
            PASSWORDS_MATCH=1
        fi
-   done ; }
+   done
+}
 
-set_log() {
-	if [ ! -d ${LOGDIR} ] ; then
+function set_log() {
+	if [ ! -d ${LOGDIR} ]; then
 		mkdir ${LOGDIR}
 		chmod -R 755 ${LOGDIR}
 	fi
 	touch ${LOGFILE}
 	chmod 755 ${LOGFILE}
 	echo -e "SETLOGTS='foo'\nUPDATETS=\nINSTALLTS=\nMAINTS=\nTIMETS=\nCLEANTS=\nFIXTS=" > ${LOGFILE}
-	#logTimestamp "SETLOGTS" ; }
+	#logTimestamp "SETLOGTS"
+}
 
 update_ubuntu(){
         #clear
@@ -99,32 +108,36 @@ update_ubuntu(){
         apt-get -y update --fix-missing
         apt-get -y upgrade
         apt-get -y dist-upgrade
-	#logTimestamp "UPDATETS" ; }
+	#logTimestamp "UPDATETS"
+}
 
 install_tools(){
         clear
         print_status "Install Tools"
         apt-get -y  install build-essential git ntp vim nano make moreutils perl gcc curl wget net-tools zsh
-	#logTimestamp "INSTALLTS" ; }
+	#logTimestamp "INSTALLTS"
+}
 
 run_main() {
     	read -rp "Enter the username of the new user account:" username
     	promptForPassword
     	read -rp "Enter the mail of git account:" gitmail
-    	
+
 	addUserAccount "${username}" "${password}"
-	
+
    	read -rp $'Paste in the public SSH key for the new user:\n' sshKey
     	echo 'Running setup script...'
-	
+
     	disableSudoPassword "${username}"
     	addSSHKey "${username}" "${sshKey}" "${gitmail}"
-	#logTimestamp "MAINTS" ; }
+	#logTimestamp "MAINTS"
+}
 
 run_time() {
     	timezone="Australia/Sydney"
     	setTimezone "${timezone}"
-	#logTimestamp "TIMETS" ; }
+	#logTimestamp "TIMETS"
+}
 
 run_clean() {
 	sudo service ssh restart
@@ -132,36 +145,35 @@ run_clean() {
 	apt-get clean
         apt-get autoclean
         apt-get -y autoremove
-	for f in .bash* .wget* .profile* .vim* ; do
-	        if [ -f ${f} ] ; then
+	for f in .bash* .wget* .profile* .vim*; do
+	        if [ -f ${f} ]; then
 			rm $f
 		fi
 	done
-	#logTimestamp "CLEANTS" ; }
+	#logTimestamp "CLEANTS"
+}
 
 run_fix(){
 	#if $username var isn't defined run prompt
         if [ -z "${username}" ] ; then
 	        read -rp "Enter the username of the new user account:" username
         fi
-	
+
 	#add $username to sudo
 	adduser ${username} sudo
-	
 	#check git-setup.sh exists in $username home folder
-        if [ ! -f /home/${username}/git-setup.sh ] ; then
-		execAsUser ${username} "wget https://github.com/ef323j3T/linux-postinstall/raw/master/user-setup.sh -P /home/${username} && chmod +x /home/${username}/git-setup.sh"
+        if [ ! -f /home/${username}/git-setup.sh ]; then
+		execAsUser ${username} "wget https://github.com/ef323j3T/linux-postinstall/raw/master/user-setup.sh -P /home/${username} && chmod +x /home/${username}/user-setup.sh"
         fi
-	
 	#change default shell
 	usermod -s /usr/bin/zsh ${username}
         execAsUser ${username} "chsh -s $(which zsh)"
-	
 	#change to user
 	cd /home/${username}
 	touch /home/${username}/.zshrc
 	#logTimestamp "FIXTS"
-	su ${username} ; }
+	su ${username}
+}
 
 #set_log
 update_ubuntu
